aa0c832fff9678061a946f2f9392a37107d88a80
horwardL <hwloh14@gmail.com>
2020-06-16 16:48:23 -0400

    fix: changes to tuple

--- catalogueApp/admin.py
+++ catalogueApp/admin.py
@@ -8,13 +8,13 @@ class CategoryAdmin(admin.ModelAdmin):
     form = CategoryAdminForm
 
     list_display = ('name', 'created_date', 'modified_date')
-    list_display_links = ('name')
+    list_display_links = ('name',)
     list_per_page = 20
     ordering = ['name']
     search_fields = ['name', 'description', 'meta_keywords', 'meta_description']
     exclude = ('created_date', 'modified_date')
 
-    prepopulated_fields = {'slug' : ('name')}
+    prepopulated_fields = {'slug' : ('name',)}
 
 
 @admin.register(Market)
@@ -22,13 +22,13 @@ class MarketAdmin(admin.ModelAdmin):
     form = MarketAdminForm
 
     list_display = ('name', 'created_date', 'modified_date')
-    list_display_links = ('name')
+    list_display_links = ('name',)
     list_per_page = 20
     ordering = ['name']
     search_fields = ['name', 'description', 'meta_keywords', 'meta_description']
     exclude = ('created_date', 'modified_date')
 
-    prepopulated_fields = {'slug' : ('name')}
+    prepopulated_fields = {'slug' : ('name',)}
 
 
 @admin.register(Product)
@@ -36,10 +36,10 @@ class ProductAdmin(admin.ModelAdmin):
     form = ProductAdminForm
 
     list_display = ('name', 'price', 'old_price', 'created_date', 'modified_date')
-    list_display_links = ('name')
+    list_display_links = ('name',)
     list_per_page = 50
     ordering = ['-created_date']
     search_fields = ['name', 'description', 'meta_keywords', 'meta_description']
     exclude = ('created_date', 'modified_date')
 
-    prepopulated_fields = {'slug' : ('name')}
+    prepopulated_fields = {'slug' : ('name',)}
