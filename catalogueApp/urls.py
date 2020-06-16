a64dd17f95633db55d5004fd499615cf97a87dda
horwardL <hwloh14@gmail.com>
2020-06-16 17:24:37 -0400

    creating url patterns and views for catalogue app

---
+++ catalogueApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from catalogueApp import views
+
+urlpatterns = [
+    path('', views.catalogue, name='catalogue'),
+    path('catalogue/<category_slug>/<markey_slug>/', views.catalogue, name='catalogue'),
+    path('catalogue/products/<product_slug>/', views.product_detail, name='product_detail'),
+]
--- catalogueApp/views.py
+++ catalogueApp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from datetime import datetime
 
-# Create your views here.
+
+def catalogue(request, catagory_slug='all-categories', market_slug='all-brands'):
+    assert isinstance(request, HttpRequest)
+
+    if request.method == 'POST':
+        return render(
+            request,
+            'catalogueApp/index.html',
+            {
+                'title': 'Product Page',
+                'year': datetime.now().year,
+            }
+        )
+    else:
+        return render(
+            request,
+            'catalogueApp/index.html',
+            {
+                'title': 'Product Page',
+                'year': datetime.now().year,
+            }
+        )
+
+
+def product_detail(args):
+    return render()
