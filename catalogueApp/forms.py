e4a7bde7249c57faef1d63e9533833aa99557249
horwardL <hwloh14@gmail.com>
2020-06-16 16:28:04 -0400

    added form for catalogue

---
+++ catalogueApp/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from catalogueApp.models import Category, Market, Product
+
+
+class CategoryAdminForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        exclude = ('created_date', 'modified_date')
+
+
+class MarketAdminForm(froms.ModelForm):
+    class Meta:
+        model = Market
+        exclude = ('created_date', 'modified_date')
+
+
+class ProductAdminForm(froms.ModelForm):
+    class Meta:
+        model = Product
+        exclude = ('created_date', 'modified_date')
+
+    def clean_price(self):
+        if self.cleaned_data['price'] <= 0:
+            raise forms.ValidationError('Price must be greater than zero.')
+        return self.cleaned_data['price']
