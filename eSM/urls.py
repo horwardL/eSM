c07f7606f5e0df1c02bd612cd7827b2ce3cf3a6d
horwardL <hwloh14@gmail.com>
2020-06-18 13:41:29 -0400

    added url pattern for cart

--- eSM/urls.py
+++ eSM/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include(('catalogueApp.urls', 'catalogueApp'), "catalogueUrls")),
+    path('', include(('cartApp.urls', 'cartApp'), "cartUrls")),
+    
     path('admin/', admin.site.urls),
 ]
