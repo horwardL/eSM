8686944672d9e744ad3e2d93636e7fa8b7b6409c
horwardL <hwloh14@gmail.com>
2020-06-18 13:48:32 -0400

    added cart view and updating eSM service

--- app/eSM_context_service.py
+++ app/eSM_context_service.py
@@ -1,11 +1,17 @@
 from eSM import settings
 from catalogueApp.models import Category, Market
+from cartApp import cart_service
 
 def eSM_context(request):
     return {
         'site_name': settings.SITE_NANE,
+
         'categories': Category.objects.filter(category_status=0),
         'markets': Market.objects.filter(market_status=0),
         'selected_category': 'all-categories',
         'selected_market': 'all-markets',
+
+        'cart_item_count': cart_service.cart_items_count(request),
+        'cart_total': cart_service.get_cart_total(request),
+        'cart_items': cart_service.get_cart_items(request),
     }
--- cartApp/views.py
+++ cartApp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from cartApp import cart_service
+from datetime import datetime
 
-# Create your views here.
+def cart_detail(request):
+    assert isinstance(request, HttpRequest)
+
+    if request.method == "POST":
+        cart_service.remove_from_cart(request)
+
+        return render(
+            request,
+            'cartApp/cart_detail.html',
+            {
+                'title': 'Cart Page',
+                'year': datetime.now().year,
+            }
+        )
+    else:
+        return render(
+            request,
+            'cartApp/cart_detail.html',
+            {
+                'title': 'Cart Page',
+                'year': datetime.now().year,
+            }
+        )
