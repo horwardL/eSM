1207ae84e0db17ecfa5c9c2b65a1476508b0d9a4
horwardL <hwloh14@gmail.com>
2020-06-19 03:53:32 -0400

    created checkout and receipt view

--- checkoutApp/views.py
+++ checkoutApp/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from datetime import datetime
+from checkoutApp.forms import CheckoutForm
+from checkoutApp.checkout_service import process_checkout
 
-# Create your views here.
+def checkout(request):
+    assert isinstance(request, HttpRequest)
+
+    if request.method == "POST":
+        result  = process_checkout(request)
+        if result:
+            return redirect('/checkout/receipt')
+        else:
+            return redirect('/checkout/receipt')
+    else:
+        form = CheckoutForm()
+        return render(
+            request,
+            'checkoutApp/checkout.html',
+            {
+                'title':'Checkout Page',
+                'year':datetime.now().year,
+                'form': form,
+                }
+        )
+
+def receipt(request):
+    assert isinstance(request, HttpRequest)
+
+    return render(
+        request,
+        'checkoutApp/receipt.html',
+        {
+            'title':'Receipt Page',
+            'year':datetime.now().year,
+        }
+    )
